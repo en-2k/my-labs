c574d35c3b58232525e610ccdacf76ccf61cc436
Igor Sinepolsky <45458643+en-2k@users.noreply.github.com>
2020-01-19 21:28:42 +0200

    upd lab1.php

--- lab1.php
+++ lab1.php
@@ -1,1 +1,18 @@
 
+<?php
+  $pageData = [
+    'title' => 'Лаби Синєпольського',
+    'activeLinks' => []
+  ];
+  for ($i = 1; $i <= 7; $i++){
+    $pageData['activeLinks']['lab' . $i] = "";
+  }
+  $pageData['activeLinks']['lab1'] = "active";
+  require "header.php";
+?>
+    <main role="main" class="container">
+      <h1>Л.р.1. Гамування. Моделювання роботи скремблера</h1>
+    </main>
+<?php
+  require "footer.php";
+?>
