c66502da5be5cc4cd575d9c0c20163a69b5c66d0
Igor Sinepolsky <45458643+en-2k@users.noreply.github.com>
2020-01-19 12:44:51 +0200

    upd index.php

--- index.php
+++ index.php
@@ -1,80 +1,16 @@
-<!doctype html>
-<html lang="uk">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <style>
-    body {
-      padding-top: 4.5rem;
-    }
-    </style>
-    <title>Лаби Синєпольського</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <a class="navbar-brand" href="https://my-labs.herokuapp.com/">Лаби</a>
-        <button class="navbar-toggler" 
-                type="button" 
-                data-toggle="collapse" 
-                data-target="#navbarCollapse" 
-                aria-controls="navbarCollapse" 
-                aria-expanded="false" 
-                aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Л.р. 1 <!--span class="sr-only">(current)</span--></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Л.р.2</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Л.р.3</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Л.р.4</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Л.р.5</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Л.р.6</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Л.р.7</a>
-          </li>
-        </ul>
-        <!--div class="form-inline mt-2 mt-md-0">
-          <div class="btn-group">
-            <button type="button" 
-                class="btn btn-danger 
-                dropdown-toggle" 
-                data-toggle="dropdown" 
-                aria-haspopup="true" 
-                aria-expanded="false">
-              Дисципліни
-            </button>
-            <div class="dropdown-menu">
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Криптографія</a>
-            </div>
-          </div>
-        </div>
-      </div-->
-    </nav>
+<?php
+  $pageData = [
+    'title' => 'Лаби Синєпольського',
+    'activeLinks' => []
+  ];
+  for ($i = 1; $i <= 7; $i++){
+    $pageData['activeLinks']['lab' . $i] = "";
+  }
+  require "header.php";
+?>
     <main role="main" class="container">
       Привіт!
     </main>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </body>
-</html>
+<?php
+  require "footer.php";
+?>
