5f9e731ce10d0dd8efb1add10d8233384b5c6eb8
Igor Sinepolsky <45458643+en-2k@users.noreply.github.com>
2020-01-20 02:01:39 +0200

    l1

--- lab1.php
+++ lab1.php
@@ -402,10 +402,10 @@ $("#text_view").change(function(){
         b += e;
       }
     }
+    console.log(b);
     for (let j = 0; j < 2 - b.length; j++){
       b += "0";
     }
-    console.log(b);
     if (b !== "00"){
         let x = CP1251[parseInt(b,16)].hexNCR;
         str1 += x;
