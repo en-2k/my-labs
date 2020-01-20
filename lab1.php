ef887b60c80baafa96bf15751114fa2178b018ff
Igor Sinepolsky <45458643+en-2k@users.noreply.github.com>
2020-01-20 14:16:55 +0200

    upd lab1.php

--- lab1.php
+++ lab1.php
@@ -383,10 +383,10 @@ $("#text_view").change(function(){
       }
     }
     let l = b.length;
-    for (let j = 0; j < charbase - l; j++){
+    for (let j = 0; j < oldcharbase - l; j++){
       b += "0";
     }
-    if (b !== zerostr){
+    if (b !== oldzerostr){
         let x = parseInt(b,oldbase).toString(base).toUpperCase();
         let l = x.length;
         for (let j = 0; j < charbase - l; j++){
